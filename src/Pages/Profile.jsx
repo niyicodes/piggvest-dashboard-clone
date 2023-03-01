ae850afb6404196d93ba2f196ea8966d45c0dc14
daydy2 <igedanielolumide@gmail.com>
2023-03-01 12:12:06 +0100

    feat: profi-pict with hover effect

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -4,17 +4,22 @@ import TopHeader from "../Components/Heading/TopHeader";
 import { Link } from "react-router-dom";
 import ProfileCard from "../Components/Card/ProfileCard";
 import cardArray from "../Components/Card/CardArray";
+import { HiOutlineCamera } from "react-icons/hi";;
+import { useNavigate } from "react-router-dom";
 
 const Profile = () => {
+  const navigate = useNavigate()
   const pocketImgLink =
     "https://storage.googleapis.com/piggybankservice.appspot.com/statics/connect-Pocket.jpg";
 
+  const profileImage =
+    "https://storage.googleapis.com/piggybankservice.appspot.com/agent_pics/logo8820da29ff.jpeg";
   return (
     <Account>
       {/* Name coming from backend. consume */}
       <TopHeader title={"My Account"} subtitle={"Name"} />
-      <section className="account">
-        <div className="div__left">
+      <main className="account">
+        <section className="div__left">
           <Link to={"/"}>
             <img
               src={pocketImgLink}
@@ -24,18 +29,32 @@ const Profile = () => {
           </Link>
           {cardArray.map((card) => {
             return (
-              
               <ProfileCard
                 icon={card.icon}
                 name={card.name}
                 color={card.color}
                 path={card.path}
               />
-            )
+            );
           })}
-        </div>
-        <div className="div__right"></div>
-      </section>
+        </section>
+        <section className="div__right">
+          <div className="right__top">
+            <div className="right__top-Img" onClick={() => navigate('/')}>
+              <img src={profileImage} alt="profile Image" srcset="" />
+
+              <div className="right__top-hover">
+                {" "}
+                <HiOutlineCamera size={28} style={{ color: "#FFFFFF" }} />{" "}
+              </div>
+            </div>
+            <span className="right__top-text">
+              <TopHeader title={"name"} style={{ margin: "0 auto" }} />
+            </span>
+          </div>
+          
+        </section>
+      </main>
     </Account>
   );
 };
@@ -43,8 +62,7 @@ const Profile = () => {
 const Account = styled.main`
   .account {
     display: flex;
-    justify-content: flex-start;
-    align-items: flex-start;
+    justify-content: space-between;
 
     .div__left {
       flex: 4;
@@ -58,6 +76,57 @@ const Account = styled.main`
     }
     .div__right {
       flex: 8;
+      margin-left: 100px;
+    }
+    .right__top {
+      &-text div {
+        margin: 0 auto;
+      }
+
+      &-Img {
+        width: 129px;
+        height: 129px;
+        margin: 0 auto;
+        border-radius: 50%;
+        position: relative;
+        overflow: hidden;
+        transition: 0.3s ease-in-out;
+        
+
+        & img {
+          object-fit: cover;
+          width: 129px;
+          height: 129px;
+          border-radius: 50%;
+          z-index: 1;
+        }
+
+
+        &:hover {
+          cursor: pointer;
+
+          .right__top-hover {
+            padding: 5px 10px;
+            background: rgba(150, 147, 137, 0.6);
+            width: 129px;
+            position: absolute;
+            margin-top: -40px;
+            display: flex;
+            justify-content: center;
+            z-index: 99;
+            
+            
+
+            & svg {
+              margin: 0 auto;
+            }
+          }
+        }
+      }
+
+      &-hover {
+        display: none;
+      }
     }
   }
 `;
