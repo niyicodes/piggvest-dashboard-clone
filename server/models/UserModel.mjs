d36806c6d8c9c933d6f6d2a23214012a9b4913d9
daydy2 <igedanielolumide@gmail.com>
2023-03-09 19:17:09 +0100

    feat: added cors middleware

--- server/models/UserModel.mjs
+++ server/models/UserModel.mjs
@@ -1,66 +1,67 @@
 import mongoose from "mongoose";
 
-const userSchema = mongoose.Schema({
-  firstname: {
-    type: String,
-    required: true,
-  },
-  lastname: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    default: "basic",
-    enum: ["basic", "admin"],
-  },
-  accDetail: [
-    {
-      accountNum: {
-        type: Number,
-        unique: true,
-      },
-      piggyPoint: {
-        type: Number,
-      },
-      refEarning: {
-        type: Number,
-      },
-      piggyLink: {
-        type: String,
-      },
+const userSchema = mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
     },
-    {
-      savings: {
-        type: Number
-      },
-      investment:{
-        type: Number
-      },
-      flexDollar: {
-        type: Number
-      },
-      flexNaira: {
-        type: Number
-      }
+    lastname: {
+      type: String,
+      required: true,
     },
-  ],
-  phoneNumber: {
-    type: String,
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      default: "basic",
+      enum: ["basic", "admin"],
+    },
+    accDetail: [
+      {
+        accountNum: {
+          type: Number,
+          unique: true,
+        },
+        piggyPoint: {
+          type: Number,
+        },
+        refEarning: {
+          type: Number,
+        },
+        piggyLink: {
+          type: String,
+        },
+      },
+      {
+        savings: {
+          type: Number,
+        },
+        investment: {
+          type: Number,
+        },
+        flexDollar: {
+          type: Number,
+        },
+        flexNaira: {
+          type: Number,
+        },
+      },
+    ],
+    phoneNumber: {
+      type: String,
+    },
   },
-}, {
-    timestamp: {
-        
-    }
-});
+  {
+    timestamp: true,
+  }
+);
 
 module.exports = mongoose.model("user", userSchema);
--- server/server.mjs
+++ server/server.mjs
@@ -1,25 +1,26 @@
 import express from "express";
 import bodyParser from "body-parser";
-import path from 'path';
+import path from "path";
 import dotenv from "dotenv";
-import {connectDB} from "./config/connection.mjs";
-import session from 'express-session'
-import { default as connectMongoDBSession} from 'connect-mongodb-session';
+import { connectDB } from "./config/connection.mjs";
+import session from "express-session";
+import { default as connectMongoDBSession } from "connect-mongodb-session";
 import errorHandler from "./middleware/errorMid.mjs";
+import router from "./route/route.mjs";
 
 dotenv.config();
-connectDB()
+connectDB();
 
 const MongoDBStore = connectMongoDBSession(session);
 
-
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader("Acces-Control-Origin", "*");
+  res.setHeader("Acces-Control-Method", "GET, POST, PUT, DELETE, PATCH");
+  res.setHeader("Acces-Control-Headers", "Content-Type, Authorization");
+});
 
-app.get('/', (req, res, next) => {
-  res.send('<h1>Hello world</h1>')
-})
+app.use(router);
 
-app.use(errorHandler)
-
-
+app.use(errorHandler);
